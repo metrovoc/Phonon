8b7b107fea2dc4cb243e50de745176a9beaa4348
metrovoc <metrovoc@users.noreply.github.com>
2025-11-25 13:57:09 +0900

    visual effect

--- common/src/main/java/com/metrovoc/phonon/block/SpeakerBlock.java
+++ common/src/main/java/com/metrovoc/phonon/block/SpeakerBlock.java
@@ -2,6 +2,8 @@ package com.metrovoc.phonon.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -9,27 +11,34 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 
 public class SpeakerBlock extends Block implements EntityBlock {
 
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
+    public static final BooleanProperty PLAYING = BooleanProperty.create("playing");
 
     public SpeakerBlock() {
         super(Properties.of()
             .strength(1.5f)
             .noOcclusion()
+            .lightLevel(state -> state.getValue(PLAYING) ? 7 : 0)
         );
-        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+        registerDefaultState(stateDefinition.any()
+            .setValue(FACING, Direction.NORTH)
+            .setValue(PLAYING, false));
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, PLAYING);
     }
 
     @Override
@@ -40,6 +49,18 @@ public class SpeakerBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new SpeakerBlockEntity(pos, state);
+    }
+
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+        if (state.getValue(PLAYING)) {
+            // Note particle above the speaker, like a jukebox
+            double x = pos.getX() + 0.5;
+            double y = pos.getY() + 1.2;
+            double z = pos.getZ() + 0.5;
+            // Color cycles through 0-24 range
+            level.addParticle(ParticleTypes.NOTE, x, y, z, random.nextInt(25) / 24.0, 0, 0);
+        }
     }
 
     @Override

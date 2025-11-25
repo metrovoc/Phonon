468f3f0d903af0379827ea4492079fabe73c5493
metrovoc <tovkaic@gmail.com>
2025-11-25 13:44:31 +0900

    block model

--- common/src/main/java/com/metrovoc/phonon/block/SpeakerBlock.java
+++ common/src/main/java/com/metrovoc/phonon/block/SpeakerBlock.java
@@ -1,25 +1,40 @@
 package com.metrovoc.phonon.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 
-import java.util.function.Supplier;
-
 public class SpeakerBlock extends Block implements EntityBlock {
+
+    public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public SpeakerBlock() {
         super(Properties.of()
             .strength(1.5f)
             .noOcclusion()
         );
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
     }
 
     @Override
